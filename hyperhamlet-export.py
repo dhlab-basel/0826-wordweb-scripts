9ce17cece1483e6eef93d069991f4c7949375fe7
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-05-24 13:33:40 +0200

    test florid

--- hyperhamlet-export.py
+++ hyperhamlet-export.py
@@ -15,19 +15,21 @@ try:
     for row in results:
         # print(row["description"])
 
-        x = re.search("(?<=b\.\s)\d{4}", row["description"])
-        y = re.search("(?<=d\.\s)\d{4}", row["description"])
+        birth = re.search("(?<=b\.\s)\d{4}", row["description"])
+        death = re.search("(?<=d\.\s)\d{4}", row["description"])
+        florid = re.search("(?<=fl\.\s)\d{4}", row["description"])
 
-        if (x):
-            print("Birth!", x.group(0), row)
-        # else:
-        #     print("No match")
+        if (birth):
+            print("Birth!", birth.group(0), row)
 
-        if (y):
-            print("Death!", y.group(0), row)
+        if (death):
+            print("Death!", death.group(0), row)
 
-        if (x and y)
-            print()
+        if (florid):
+            print("Florid!", florid.group(0), row)
+
+        # if birth and death:
+        #     print("Same")
 
     conn.close()
     cursor.close()
