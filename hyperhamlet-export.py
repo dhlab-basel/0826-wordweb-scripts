441398522cbd810fc73032b3526e561f82a6eb90
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-05-24 11:49:12 +0200

    include regex

--- hyperhamlet-export.py
+++ hyperhamlet-export.py
@@ -1,15 +1,34 @@
 import pymysql
+import re
 
 try:
     conn = pymysql.connect(host='localhost', user='vitsch', password='test', database='HAMLET')
 
     cursor = conn.cursor(pymysql.cursors.DictCursor)
+
     sql = "SELECT * FROM authors"
 
     cursor.execute(sql)
+
     results = cursor.fetchall()
+
     for row in results:
-        print(row["lastname"])
+        # print(row["description"])
+
+        x = re.search("(?<=b\.\s)\d{4}", row["description"])
+        y = re.search("(?<=d\.\s)\d{4}", row["description"])
+
+        if (x):
+            print("Birth!", x.group(0), row)
+        # else:
+        #     print("No match")
+
+        if (y):
+            print("Death!", y.group(0), row)
+
+        if (x and y)
+            print()
+
     conn.close()
     cursor.close()
 except Exception as err:
