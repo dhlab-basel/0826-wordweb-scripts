bda5f4d61e6fed4b401204b1701e4a6a84147418
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-06-04 17:13:57 +0200

    hard coded bulk import

--- wordweb_create-resource.py
+++ wordweb_create-resource.py
@@ -4,7 +4,8 @@ from pprint import pprint
 import argparse
 import json
 from jsonschema import validate
-from knora import KnoraError, knora
+from knora import KnoraError, knora, BulkImport
+import requests
 
 
 parser = argparse.ArgumentParser()
@@ -20,12 +21,12 @@ args = parser.parse_args()
 con = knora(args.server, args.user, args.password)
 schema = con.create_schema(args.projectcode, args.ontoname)
 
-res_info1 = con.create_resource(schema, "book", "test-book", {
-    "title": "Romeo und Julia"
-})
+#res_info1 = con.create_resource(schema, "book", "test-book", {
+#    "title": "Romeo und Julia"
+#})
 
-pprint(res_info1)
-
+#pprint(res_info1)
+#
 # res_info2 = con.create_resource(schema, "person", "test-person", {
 #     "internalID": "&000001",
 #     "firstName": "William",
@@ -33,10 +34,10 @@ pprint(res_info1)
 #     "description": "English Dramatist",
 #     "birthDate": "GREGORIAN:1564",
 #     "deathDate": "GREGORIAN:1616",
-#     "isAuthorOf": "http://rdfh.ch/0826/ErPFeUnJSOKce3M3Tl73dw"
+#     "isAuthorOf": "http://rdfh.ch/0826/I2xQrsXYSnuDARYIH772Eg"
 # })
-#
-# pprint(res_info2)
+
+#pprint(res_info2)
 #
 # res_info3 = con.create_resource(schema, "person", "test-person", {
 #     "internalID": "@000001",
@@ -45,3 +46,67 @@ pprint(res_info1)
 # })
 #
 # pprint(res_info3)
+
+
+ww_bulk_xml = "./test-bulk-output.xml"
+ww_bulk_object = BulkImport(schema)
+
+buch = {}
+buch["title"] = "Romeo und Julia"
+
+person = {
+    "internalID": "&000001",
+    "firstName": "William",
+    "lastName": "Shakespeare",
+    "description": "English Dramatist",
+    "birthDate": "GREGORIAN:1564",
+    "deathDate": "GREGORIAN:1616",
+    "isAuthorOf": "B_01",
+    "hasSex": "male"
+}
+
+edition = {
+    "isEditionOf": "B_01",
+    "isWrittenIn": "L_01"
+}
+
+language = {
+    "languageName": "Deutsch"
+}
+
+ww_bulk_object.add_resource(
+    "language",
+    "L_01",
+    "sprache label",
+    language
+)
+
+ww_bulk_object.add_resource(
+    "book",
+    "B_01",
+    "buch label",
+    buch
+)
+
+ww_bulk_object.add_resource(
+    "person",
+    "P_01",
+    "person label",
+    person
+)
+
+ww_bulk_object.add_resource(
+    "edition",
+    "E_01",
+    "edition label",
+    edition
+)
+
+
+BULKIMPORT_API_ENDPOINT="http://localhost:3333/v1/resources/xmlimport/http%3A%2F%2Frdfh.ch%2Fprojects%2F0826"
+headers = {"Content-Type": "application/xml"}
+
+ww_bulk_object.write_xml(ww_bulk_xml)
+ww_bulk_xml_string = open(ww_bulk_xml).read().encode("utf-8")
+r = requests.post(BULKIMPORT_API_ENDPOINT, data=ww_bulk_xml_string, headers=headers, auth=(args.user, args.password))
+pprint(r.json())
