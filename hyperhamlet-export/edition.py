5ea1f50bfdcfd6a2454755554da776c65abb876a
Vijeinath Tissaveerasingham <vijeinath@gmx.ch>
2019-06-14 17:47:21 +0200

    integrating pandas (not animals)

--- hyperhamlet-export/edition.py
+++ hyperhamlet-export/edition.py
@@ -1,12 +1,14 @@
 import csv
 import re
 import pymysql
+import pandas
 
 with open('export.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     line_count = 0
     copied = 0
     pi = {}
+    data = []
     for row in csv_reader:
 
         publicationInfo = re.search("(.+)\s\s(.+)\s\s(.+)", row[4])
@@ -14,9 +16,11 @@ with open('export.csv') as csv_file:
             # print(line_count, publicationInfo.groups())
             letterName = re.search("\"(.+)\"\s(.*)", publicationInfo.group(2))
             if letterName:
-                print(line_count, "author: " + publicationInfo.group(1) + " | lettername: " + letterName.group(1) + " | pubInfo: " + letterName.group(2) + " | page: " + publicationInfo.group(3))
+                data.append([line_count, publicationInfo.group(1), letterName.group(1), letterName.group(2), publicationInfo.group(3)])
+                # print(line_count, "author: " + publicationInfo.group(1) + " | lettername: " + letterName.group(1) + " | pubInfo: " + letterName.group(2) + " | page: " + publicationInfo.group(3))
             else:
-                print(line_count, "author: " + publicationInfo.group(1) + " | pubInfo: " + publicationInfo.group(2) + " | page: " + publicationInfo.group(3))
+                data.append([line_count, publicationInfo.group(1), None, publicationInfo.group(2), publicationInfo.group(3)])
+                # print(line_count, "author: " + publicationInfo.group(1) + " | pubInfo: " + publicationInfo.group(2) + " | page: " + publicationInfo.group(3))
             # if publicationInfo.group(2) in pi:
             #     cla = 0
             # else:
@@ -32,13 +36,21 @@ with open('export.csv') as csv_file:
             if onlyPubInfo:
                 lName = re.search("\"(.+)\"\s(.*)", onlyPubInfo.group(2))
                 if lName:
-                    print(line_count, "author: " + onlyPubInfo.group(1) + " | lettername: " + lName.group(1) + " | pubInfo: " + lName.group(2))
+                    data.append([line_count, onlyPubInfo.group(1), lName.group(1), lName.group(2), None])
+                    # print(line_count, "author: " + onlyPubInfo.group(1) + " | lettername: " + lName.group(1) + " | pubInfo: " + lName.group(2))
                 else:
-                    print(line_count, "author: " + onlyPubInfo.group(1) + " | pubInfo: " + onlyPubInfo.group(2))
+                    data.append([line_count, onlyPubInfo.group(1), None, onlyPubInfo.group(2), None])
+                    # print(line_count, "author: " + onlyPubInfo.group(1) + " | pubInfo: " + onlyPubInfo.group(2))
             else:
                 print(line_count, "FAIL")
         line_count += 1
     print("line count", line_count)
+
+    df = pandas.DataFrame(data, columns=['Number', 'Author', 'LetterName', 'PubInfo', 'Page'])
+    pandas.set_option('display.max_rows', None)
+    pandas.set_option('display.max_colwidth', 100)
+
+    print(df)
 
     # Print all the values in the object
     # for b in pi:
